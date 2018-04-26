380315b2b5e68dce1db8f309ccf8e3033bb4c85e
Roman Luntsevich <shipik2008@gmail.com>
2018-04-26 19:59:20 +0300

    Catch key arrow

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -1,3 +1,7 @@
+
+
+var scrollStep = 280;
+
 $(function () {
     $(window).scroll(function () {
         var winTop = $(window).scrollTop();
@@ -11,4 +15,21 @@ $(function () {
 
 $('.product-table-container').scroll(function(){
     $(".compare__sticky-body").scrollLeft($('.product-table-container').scrollLeft());
+});
+
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 37: // left
+        $('.product-table-container').animate({ scrollLeft: '-=280' }, 250);
+        console.log($('.product-table-container').scrollLeft());
+        break;
+        
+        case 39: // right
+        $('.product-table-container').animate({ scrollLeft: '+=280' }, 250);
+        console.log($('.product-table-container').scrollLeft());
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
 });
